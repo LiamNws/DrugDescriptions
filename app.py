7ce5f8ddc3e86fd00a77f7740242b85bfed02fad
LiamNws <liamnewson83@gmail.com>
2023-09-23 12:45:41 +0100

    removed double spaces from final output sentence cleaned up

--- app.py
+++ app.py
@@ -1,30 +1,37 @@
 import pandas as pd
-import openpyxl
 import os
 import pyperclip
+import re
 
 excel = os.path.join("Source", "DrugDescriptions.xlsx")
 
 try:
     df = pd.read_excel(excel)
 except FileNotFoundError:
-    print("File not found")
+    print("File not found.")
     exit(1)
 except Exception as e:
     print("An error has occurred while reading the file")
     exit(1)
 
 startstop = True
+
 while startstop:
 
-    user_input = input("Enter Drug Name: ")
+    user_input = input("Enter Drug Name (e.g metformin 500): ")
 
     matching = df[df["Drug Name"].str.contains(user_input, case=False, na=False)]
 
     if not matching.empty:
         for i, row in matching.iterrows():
+
             form = row["Form"]
-            shape = row["Shape"]
+
+            if pd.isna(row["Shape"]) or row["Shape"] == "":
+                shape = ""
+            else:
+                shape = row["Shape"]
+
             colour = row["Colour"]
 
             if row["Imprint"] == "none":
@@ -37,13 +44,17 @@ while startstop:
             name = row["Drug Name"]
 
             sentence1 = f"{name}: {colour} {shape} {form}, '{imprint}' imprinted. \n -LN"
+            sentence1 = re.sub(r"(?<!\n)\s+", " ", sentence1)
             sentence2 = f"{name}: {colour} {shape} {form}, no imprint. \n -LN"
+            sentence2 = re.sub(r"(?<!\n)\s+", " ", sentence2)
 
             if imp_found:
                 print(sentence1)
+                # print("Copied to clipboard.")
                 pyperclip.copy(sentence1)
             elif not imp_found:
                 print(sentence2)
+                # print("Copied to clipboard.")
                 pyperclip.copy(sentence2)
 
 
